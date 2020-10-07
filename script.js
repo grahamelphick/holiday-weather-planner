5bae6e6919d0b2b85ef8539a4bc32fc08a261761
Julia <daubertjulia@yahoo.com>
2020-10-07 15:05:59 +0800

    "Updated HTML to suit current Activity API"

--- script.js
+++ script.js
@@ -4,43 +4,52 @@ $(document).ready(function () {
     //console.log("hello World");
 
     //API Key
-    var apiKey = "AIzaSyD51Xnuhy0Fa--8C553LeKhC0F-ZywEDDE"
+    //var apiKey = "fQl4uruJbRL1M63hlwlIHhzLJM-rEFeK6InNrpXx5o0"
 
-    let locationName = document.getElementById("locationInput");
+    //var baseURL = "https://places.ls.hereapi.com/places/v1"
+
+    //var action = "/discover/explore"
+    //var URL = baseURL + action + "?cat=sights-museums&at=52.531,13.3843&apiKey=" + apiKey
 
 
+    function requestData(what) {
+        $.ajax({
+            url: 'https://places.ls.hereapi.com/places/v1/discover/explore',
+            type: 'GET',
+            data: {
+                at: '52.5159,13.3777',
+                cat: what,
+                apiKey: 'fQl4uruJbRL1M63hlwlIHhzLJM-rEFeK6InNrpXx5o0'
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Accept', 'application/json');
+            },
+            success: function (data) {
+                console.log(data);
+                populateResults(data);
+            }
+        });
+    }
 
+
+    function populateResults(data) {
+        html= "";
+        console.log(data);
+        data.results.items.forEach(element => {
+            html+=element.title+"<br>";
+        });
+        $("#results").html(html)
+    }
 
     //On Click function for starting the search based on input
     $(".searchbtn").on("click", function () {
         console.log("btn clicked");
-        //let userLocation = locationName.value;
-        //console.log(userLocation);
-
-        var userLocation = "Perth";
-        let queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + userLocation + "%20Museum%20&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=" + apiKey
-
-       //https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyD51Xnuhy0Fa--8C553LeKhC0F-ZywEDDE
-
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-            dataType: 'jsonp',
-        })
-
-            .then(function (response) {
-                var results = response.data;
-
-                console.log(results);
-
-
-            })
-
-
-
-
-
-
-    })
+        var what = $("#WhatText").val();
+        console.log(what);
+        requestData(what);
+    });
 
 })
+
+
+
