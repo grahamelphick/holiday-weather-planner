ecfbb31d66a03ed58c371318cacce111db25129e
Gragit <gme2468@gmail.com>
2020-10-12 15:23:11 +0800

    attempt 2 to fix preventdefault bug

--- buttonLocation.js
+++ buttonLocation.js
@@ -36,10 +36,10 @@ $(document).ready(function () {
         console.warn(`ERROR(${err.code}): ${err.message}`);
         $('.modal').modal();
         $('#modal1').modal('open');
+        err.preventDefault();
         // alert("Sorry, this app requires your location to work. Please allow access to your location.");
     }
-    $("#agreebtn").click(function(event){
-        event.preventDefault();
+    $("#agreebtn").click(function(){
         window.location.reload();
     });
     navigator.geolocation.getCurrentPosition(success, error, options);
